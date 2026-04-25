4cedb6c7048ad35faf3c572eee5149429ec1733d
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-04-25 16:00:50 +0800

    socket option

--- warmup/echoserver.py
+++ warmup/echoserver.py
@@ -1,4 +1,4 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from message import send_message, recv_message
 from threading import Thread
 
@@ -14,6 +14,10 @@ def echo_message(sock):
 
 def main(addr):
     sock = socket(AF_INET, SOCK_STREAM)
+    # common problem with sockets is error: "Address already in use."
+    # can be suppressed by setsockopt()
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
+
     sock.bind(addr)
     sock.listen()
     while True:
